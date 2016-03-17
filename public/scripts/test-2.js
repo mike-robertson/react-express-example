acd3f2115aa7dc02672cf0fde6e8a1e514e2e542
Mike Robertson <michael.robertson@kroger.com>
2016-03-17 16:20:51 -0400

    Mike | Removed uncessary old files

--- public/scripts/test-2.js
+++
@@ -1,9 +0,0 @@
-import {TapeMachine} from "./test-class.js";
-
-const tp = new TapeMachine();
-tp.record("Hello... Hellooooo!!! Helloooooo!!!!!");
-tp.play();
-
-const p = document.createElement("p");
-p.innerText = "Victory!";
-document.querySelector("body").appendChild(p);
